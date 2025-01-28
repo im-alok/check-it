1322f655a65b3edd7bfc6eb9172f4038d80d9a64
im-alok <alok.ranjan.aiml25@heritageit.edu.in>
2025-01-29 00:37:09 +0530

    fetch todo

---
+++ app/api/todos/getusertasklist/route.ts
@@ -0,0 +1,48 @@
+//create an api to fetch all the tasks associated to user
+
+import { prisma } from "@/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { Session } from "node:inspector";
+
+
+declare module 'next-auth' {
+    interface Session {
+        user: {
+            id: string;
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+        }
+    }
+}
+
+export async function GET(req: NextRequest) {
+    try {
+        const session = await getServerSession();
+
+        const userId = session?.user?.id
+
+        // get all usertask
+
+        const userTaskDetails = await prisma.tODO.findMany({
+            where:{
+                userId:userId
+            },
+            include:{
+                task:true
+            }
+        })
+
+        return NextResponse.json({
+            success: true,
+            message: 'User task fetched successfully',
+            userTask: userTaskDetails
+        }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong'
+        }, { status: 500 })
+    }
+}
